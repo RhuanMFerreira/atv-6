15691d12aa3db54b5e430b5510f790ed85b7ddc5
RhuanMFerreira <134220804+RhuanMFerreira@users.noreply.github.com>
2023-09-04 10:30:24 -0300

    atv--3.c

--- atv/atv--3.c
+++ atv/atv--3.c
@@ -1,64 +1,64 @@
 #include <Stdio.h>
 
 main() {
-    int Prato, Sobremesa, Bebida;
-    float TotalCalorias = 0;
+    int prato, sobremesa, bebida;
+    float totalCalorias = 0;
 
     printf("Escolha o Prato(1 - Vegetariano, 2 - Peixe, 3 - Frango, 4 - Carne):");
-    scanf("%d", &Prato);
+    scanf("%d", &prato);
 
     printf("Escolha a Sobremesa(1 - Abacaxi, 2 - Sorvete Diet, 3 - Mouse diet, 4 - Mouse chocolate):");
-    scanf("%d", &Sobremesa);
+    scanf("%d", &sobremesa);
 
     printf("Escolha a bebida(1 - Cha, 2 - Suco de Laranja, 3 - Suco de Melao, 4 - Refrigerante Diet):");
-    scanf("%d", &Bebida);
+    scanf("%d", &bebida);
 
-    switch (Prato){
+    switch (prato){
         case 1:
-         TotalCalorias += 180;
+         totalCalorias += 180;
          break;
 
         case 2:
-         TotalCalorias += 230;
+         totalCalorias += 230;
          break;
 
         case 3:
-         TotalCalorias += 250;
+         totalCalorias += 250;
          break;
 
         case 4:
-         TotalCalorias += 350;
+         totalCalorias += 350;
          break;
 
         default:
-         printf("Prato Invalido.\n");
+         printf("prato Invalido.\n");
          return 1;
     }
 
-    switch (Sobremesa){
+    switch (sobremesa){
         case 1:
-         TotalCalorias += 75;
+         totalCalorias += 75;
          break;
 
         case 2:
-         TotalCalorias += 110;
+         totalCalorias += 110;
          break;
 
         case 3:
-         TotalCalorias += 170;
+         totalCalorias += 170;
          break;
 
         case 4:
-         TotalCalorias += 200;
+         totalCalorias += 200;
          break;
 
         default:
-         printf("Sobremesa Invalida.\n");
+         printf("sobremesa Invalida.\n");
          return 1;
 
     }
 
-    switch (Bebida){
+    switch (bebida){
         case 1:
          TotalCalorias += 20;
          break;
@@ -83,5 +83,5 @@ main() {
 
     printf("\n---------Suas Calorias----------\n");
 
-    printf("Total de Calorias da Refeicao e: %.2f calorias\n", TotalCalorias);
+    printf("Total de Calorias da Refeicao e: %.2f calorias\n", totalCalorias);
 }
